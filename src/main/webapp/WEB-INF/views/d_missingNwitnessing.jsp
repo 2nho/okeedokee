390402fd85cb36ac6cb19d708169f61c3002af1c
honii <honii@DESKTOP-M3T8USU>
2022-01-20 12:54:01 +0900

    박소현_missingNwitnessing_commit

--- src/main/webapp/WEB-INF/views/d_missingNwitnessing.jsp
+++ src/main/webapp/WEB-INF/views/d_missingNwitnessing.jsp
@@ -5,41 +5,12 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-
-<style type="text/css">
-/* 메인 */
-main {
-	display: flex;
-	flex-direction: row;
-}
-
-main nav {
-	flex: 3;
-}
-
-main section {
-	flex: 9;
-	width: 100%;
-	height: 100%;
-}
-
-/* 네비게이션 */
-main nav #mnwNav {
-	width: 90%;
-	height: 350px;
-	background-color: #f9f1c0;
-}
-main nav #mnwNav #mnwTitle::after {
-	content: "";
-	display: block;
-	width: 90%;
-	border-bottom: 2px solid black;
-}
-</style>
+<link rel="stylesheet" href="css/d_missingNwitnessing.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
 
-<div class="totalContainer">
+<div class="container">
 	<!-- 헤더 불러오기 -->
 	<jsp:include page="/WEB-INF/views/layout/header.jsp"/>
 	
@@ -48,37 +19,82 @@ main nav #mnwNav #mnwTitle::after {
 			<!-- missing and witnessing 줄여서 mnw -->
 			<div id="mnwNav">
 				<div id="mnwTitle">
-					<h4>유기견 실종 / 목격 신고</h4>
+					<h3>유기견 실종 / 목격 신고</h3>
 				</div>
 				<div id="mnwSubTitle">
-					<h5>유기견 실종 신고</h5>
-					<h5>유기견 목격 신고</h5>
-					<h5>셀프 전단지</h5>
+					<a href=""><h4>유기견 실종 신고</h4></a>
+					<a href=""><h4>유기견 목격 신고</h4></a>
+					<a href=""><h4>셀프 전단지</h4></a>
 				</div>
 			</div>
 		</nav>
 		<section>
 			<article id="arti1">
+			<!-- 게시글 내용 슬라이드 -->
 				<div id="slideBox">
-					<div class="slides" id="slide1"></div>
-					<div class="slides" id="slide2"></div>
-					<div class="slides" id="slide3"></div>
-					<div class="slides" id="slide4"></div>
-					<div class="slides" id="slide5"></div>
+					<div class="slides" id="slide1">
+						<div class="slideContent">
+							<img src="media/logo/mainLogo.png" alt="" />
+							<div class="text">
+								<h3>1제목제목제목제목제목제목제목제목제목</h3>
+							</div>
+						</div>
+					</div>
+					<div class="slides" id="slide2">
+						<div class="slideContent">
+							<img src="media/logo/mainLogo.png" alt="" />
+							<div class="text">
+								<h3>2제목제목제목제목제목제목제목제목제목</h3>
+							</div>
+						</div>
+					</div>
+					<div class="slides" id="slide3">
+						<div class="slideContent">
+							<img src="media/logo/mainLogo.png" alt="" />
+							<div class="text">
+								<h3>3제목제목제목제목제목제목제목제목제목</h3>
+							</div>
+						</div>
+					</div>
+					<div class="slides" id="slide4">
+						<div class="slideContent">
+							<img src="media/logo/mainLogo.png" alt="" />
+							<div class="text">
+								<h3>4제목제목제목제목제목제목제목제목제목</h3>
+							</div>
+						</div>
+					</div>
+					<div class="slides" id="slide5">
+						<div class="slideContent">
+							<img src="media/logo/mainLogo.png" alt="" />
+							<div class="text">
+								<h3>5제목제목제목제목제목제목제목제목제목</h3>
+							</div>
+						</div>
+					</div>
 				</div>
 			</article>
-
+			<div class="buttonBox">
+				<button id="prev">◀◀</button>
+				<button id="next">▶▶</button>
+			</div>
+			<!-- 게시판 -->
 			<article id="arti2">
 				<div id="missingBoard">
 					<h1>유기견 실종 신고 게시판</h1>
-					<div id="board"></div>
+					<div id="board">
+						
+					</div>
 				</div>
 			</article>
+			
 		</section>
 	</main>
 	
 	<!-- 푸터 불러오기 -->
 	<jsp:include page="/WEB-INF/views/layout/footer.jsp"/>
 </div>
+
+<script type="text/javascript" src="js/d_missingNwitnessing.js"></script>
 </body>
 </html>
---
+++ src/main/webapp/resources/js/d_missingNwitnessing.js
@@ -0,0 +1,140 @@
+/**
+ * 
+ */
+//무한 슬라이드 & 오토 슬라이드
+function slideShow() {
+	let slideBox = document.querySelector("#slideBox");
+	let slide = document.querySelectorAll(".slides");
+	let num = 0;
+	let slideCount = slide.length;
+	let slideWidth = 250; /*width: 230, padding: 10, margin: 10*/
+	let slideMargin = 50;
+	let prev = document.querySelector("#prev");
+	let next = document.querySelector("#next");
+	
+
+	/*무한슬라이드 위한 복사본 만들기*/
+	makeClone();
+
+	function makeClone() {
+		
+		for (let i = 0; i < slideCount; i++) {
+			//x.cloneNode() : x복사
+			//x.cloneNode(true) : x와 x자식까지 복사
+			let cloneSlide = slide[i].cloneNode(true);
+			//나중에 clone으로 선택하기 위해 클래스 추가
+			cloneSlide.classList.add("clone");
+			//slideBox뒤에 클론 추가
+			slideBox.append(cloneSlide);
+		}
+		
+		for (let i = slideCount-1; i >= 0; i--) {
+			//x.cloneNode() : x복사
+			//x.cloneNode(true) : x와 x자식까지 복사
+			let cloneSlide = slide[i].cloneNode(true);
+			//나중에 clone으로 선택하기 위해 클래스 추가
+			cloneSlide.classList.add("clone");
+			//slideBox앞에 클론 추가
+			slideBox.prepend(cloneSlide);
+		}
+		
+		//클론 후 전체 너비 업데이트
+		updateWidth();
+		//초기위치 설정
+		setInitialPos();
+		//slide 이동 애니메이션
+		//화면 로드시 초기위치 설정이 보이지 않도록 1초 후 클래스 추가
+		setTimeout(function(){
+			slideBox.classList.add("animated");
+		}, 100);
+		
+
+	}
+	
+	function updateWidth() {
+		//클론 후 갯수 체크
+		let currentSlides = document.querySelectorAll(".slides");
+		let newSlideCount = currentSlides.length;
+		
+		//토탈 너비 체크 후 slideBox 너비 수정하기
+		let newWidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + "px";
+		slideBox.style.width = newWidth;
+	}
+	
+	function setInitialPos() {
+		//slideCount는 초기 설정한 5개
+		//하지만 이동방향 왼쪽으로 -방향
+		let initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
+		console.log(initialTranslateValue);
+			
+		//slideBox {transform:translateX(-px)}
+		slideBox.style.transform = `translateX(${initialTranslateValue}px)`;
+	}
+
+	//버튼으로 슬라이드 이동
+	next.addEventListener("click", function(){
+		moveSlide(num+1);
+	});
+	prev.addEventListener("click", function(){
+		moveSlide(num-1);
+	});
+	
+	function moveSlide(n) {
+		slideBox.style.left = -n * (slideWidth + slideMargin) + "px";
+		//최초 숫자 변경
+		num = n;
+		console.log(num, slideCount);
+		
+		//최초숫자가 클론 제외한 slideCount랑 같으면
+		if(num == slideCount || num == -slideCount) {
+			//미리 설정한 animated효과 빼 
+			//첫 번째 슬라이드가 보인 후에 설정하기 위해 애니메이션 시간인 .5초 이후
+			//같은 번호 연속이기 때문에 slide후에는 변경되어도 바뀐 줄 모름! 
+			setTimeout(function(){
+				//초기설정 감추는 애니메이션 지우기
+				slideBox.classList.remove("animated");
+				//left 원위치로 돌리기
+				slideBox.style.left = "0px";
+				//최초숫자 원상복귀
+				num = 0;
+			}, 500);
+			
+			//다시 슬라이드 필요하므로 뺐던 애니메이션 다시 추가
+			//but 500보다 0.1초 느리게 둬서 뻑나지 않도록!
+			setTimeout(function(){
+				slideBox.classList.add("animated");
+			}, 550);
+			
+		}
+	}
+
+	//오토 슬라이드
+	let timer = undefined;
+	
+	function autoSlide() {
+		if(timer == undefined) {
+			timer = setInterval(function(){
+				moveSlide(num+1);
+			}, 3000);
+		}
+	}autoSlide();
+	
+
+	function stopSlide() {
+		clearInterval(timer);
+		//undefined일때만 autoSlide() 작동하므로 추가!
+		//미설정시 timer가 int값으로 변해 슬라이드가 빨라지는 이상움직임 있음
+		timer = undefined;
+	}
+	
+	//슬라이드 호버시 stopSlide()
+	slideBox.addEventListener("mouseenter", function(){
+		stopSlide()
+	});
+	//슬라이드 리브시 autoSlide();
+	slideBox.addEventListener("mouseleave", function(){
+		autoSlide();
+	});
+
+
+} slideShow();
