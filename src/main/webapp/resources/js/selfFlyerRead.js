df3a8b3808176e2836cbd049d82056e7e1028e8f
honii <honii@DESKTOP-M3T8USU>
2022-01-25 20:58:02 +0900

    박소현_commit

--- src/main/webapp/resources/js/selfFlyerRead.js
+++ src/main/webapp/resources/js/selfFlyerRead.js
@@ -1,15 +1,19 @@
 /**
  * 
  */
+ //전단지 프린트
 function printFlyer() {
 	const printBtn = document.querySelector("#printBtn");
+
+	printBtn.addEventListener("click", function() {
 	
-	printBtn.addEventListener("click", function(){
-		window.onbeforeprint = function() {
-			document.body.innerHTML = document.querySelector("#flyerContent");
-		}
-		
+		//프린트
 		window.print();
+		//페이지 새로고침
+		location.reload();
+
 	});
 
 } printFlyer();
+
+
