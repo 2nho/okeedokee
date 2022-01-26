7762eeaaad49be0a67037c078048b05e0c7d1fa7
2nho <kim2nho@gmail.com>
2022-01-26 14:42:38 +0900

    기부액

---
+++ src/main/java/kr/co/goodee39/vo/DonationVO.java
@@ -0,0 +1,32 @@
+package kr.co.goodee39.vo;
+
+public class DonationVO {
+	private int num;
+	private int mnum;
+	private String date;
+	private String donation;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getMnum() {
+		return mnum;
+	}
+	public void setMnum(int mnum) {
+		this.mnum = mnum;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDonation() {
+		return donation;
+	}
+	public void setDonation(String donation) {
+		this.donation = donation;
+	}
+}
