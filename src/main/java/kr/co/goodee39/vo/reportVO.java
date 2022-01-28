9ee65ee17b6803cba715e316fbd395e65014d16d
2nho <kim2nho@gmail.com>
2022-01-28 16:04:36 +0900

    int start,conut bnum 추가

--- src/main/java/kr/co/goodee39/vo/reportVO.java
+++ src/main/java/kr/co/goodee39/vo/reportVO.java
@@ -4,108 +4,80 @@ public class reportVO {
 
 	private int num;
 	private int mnum;
-	private String id;
 	private String title;
 	private String content;
 	private String createdate;
 	private int bdiv;
 	private String status;
 	private String del;
-	
-	
-	public reportVO() {
-		// TODO Auto-generated constructor stub
-	}
-
-
+	private int bnum;
+	private int start;
+	private int count;
 	public int getNum() {
 		return num;
 	}
-
-
 	public void setNum(int num) {
 		this.num = num;
 	}
-
-
 	public int getMnum() {
 		return mnum;
 	}
-
-
 	public void setMnum(int mnum) {
 		this.mnum = mnum;
 	}
-
-	public String getId() {
-		return id;
-	}
-
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
 	public String getTitle() {
 		return title;
 	}
-
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-
 	public String getContent() {
 		return content;
 	}
-
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
-
 	public String getCreatedate() {
 		return createdate;
 	}
-
-
 	public void setCreatedate(String createdate) {
 		this.createdate = createdate;
 	}
-
-
 	public int getBdiv() {
 		return bdiv;
 	}
-
-
 	public void setBdiv(int bdiv) {
 		this.bdiv = bdiv;
 	}
-
-
 	public String getStatus() {
 		return status;
 	}
-
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
-
 	public String getDel() {
 		return del;
 	}
-
-
 	public void setDel(String del) {
 		this.del = del;
 	}
-	
-	
+	public int getBnum() {
+		return bnum;
+	}
+	public void setBnum(int bnum) {
+		this.bnum = bnum;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
 	
 }
