836b60b2a6cb19334a28d0cb9510c713945ada4e
hanmi <hanmi@DESKTOP-4ADOASQ>
2022-02-03 22:27:08 +0900

    이제승_commit

--- src/main/webapp/resources/db/member.sql
+++ src/main/webapp/resources/db/member.sql
@@ -5,15 +5,14 @@ CREATE TABLE `member` (
 	`pw` VARCHAR(40) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
 	`address` VARCHAR(50) NOT NULL COLLATE 'utf8mb3_general_ci',
 	`email` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`phNum` VARCHAR(11) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+	`phNum` VARCHAR(15) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
 	`petown` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Y/N' COLLATE 'utf8mb3_general_ci',
-	`size` VARCHAR(5) NULL DEFAULT NULL COMMENT '대형, 중형, 소형' COLLATE 'utf8mb3_general_ci',
-	`level` VARCHAR(10) NULL DEFAULT NULL COMMENT '관리자, 회원' COLLATE 'utf8mb3_general_ci',
+	`size` VARCHAR(5) NULL DEFAULT NULL COMMENT 'b, m, s, e' COLLATE 'utf8mb3_general_ci',
+	`level` VARCHAR(10) NULL DEFAULT NULL COMMENT 'admin,user' COLLATE 'utf8mb3_general_ci',
 	`signupDay` DATE NULL DEFAULT NULL,
 	`del` VARCHAR(1) NULL DEFAULT 'N' COMMENT 'Y/N' COLLATE 'utf8mb3_general_ci',
 	PRIMARY KEY (`mnum`) USING BTREE
 )
 COLLATE='utf8mb3_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=6
 ;
